15b32a01e30faef426cafd6bc985866d0218b52e
Jefferson Tiago <jtandrelevicius@gmail.com>
2022-03-23 17:22:29 -0400

    add test httpRequest or and body is empyt

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,7 +1,13 @@
 // moved left
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest) {
+      return {
+        statusCode: 500
+      }
+    }
+    const { email, password } = httpRequest.body
+    if (!email || !password) {
       return {
         statusCode: 400
       }
@@ -10,15 +16,37 @@ class LoginRouter {
 }
 
 describe('Login Router', () => {
-  test('Should return 400 if no email is provider', () => {
+  test('Should return 400 if no email is provided', () => {
     const sut = new LoginRouter()
     const httpRequest = {
       body: {
         password: 'any_password'
       }
     }
-
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no password is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 500 if no httpRequest id provided', () => {
+    const sut = new LoginRouter()
+    const httpResponse = sut.route()
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Shoul return 500 if httpRequest has no body', () => {
+    const sut = new LoginRouter()
+    const httpResponse = sut.route()
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
